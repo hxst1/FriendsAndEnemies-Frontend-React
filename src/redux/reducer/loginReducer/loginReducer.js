1d5884c405038063d972d14b2bc3fdaa26be5a7c
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-26 00:12:46 +0100

    :zap: loginReducer added

---
+++ src/redux/reducer/loginReducer/loginReducer.js
@@ -0,0 +1,16 @@
+import actionTypes from "../../actions/actionTypes";
+
+const loginReducer = (currentToken = {}, action = {}) => {
+  let newToken;
+  switch (action.type) {
+    case actionTypes.login:
+      newToken = [...action.token];
+      break;
+
+    default:
+      newToken = [...currentToken];
+  }
+  return newToken;
+};
+
+export default loginReducer;
